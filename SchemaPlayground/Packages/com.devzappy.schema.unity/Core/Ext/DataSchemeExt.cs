a414328d9a0bd9ac555fc004fbe4c534608f0834
Zack Asofsky <3746793+zappy-dev@users.noreply.github.com>
2025-10-22 15:23:04 -0400

    Release v0.3.1, patch hotfix, fix compilation issue in v0.2 Codegen files due to v0.3 API change for `DataScheme.SetDataOnEntry()` (#16)

---
+++ SchemaPlayground/Packages/com.devzappy.schema.unity/Core/Ext/DataSchemeExt.cs
@@ -0,0 +1,17 @@
+﻿using Schema.Core.Data;
+
+namespace Schema.Core
+{
+    public static class DataSchemeExt
+    {
+        /// <remark>This extension is here to support an API update to DataScheme.SetDataOnEntry between v0.2 to v0.3.
+        /// This API update caused v0.2 code-gen'd files to get out-of-sync with Schema v0.3's API
+        /// </remark>
+        public static SchemaResult SetDataOnEntry(this DataScheme scheme, DataEntry entry, string attributeName, object value,
+            bool allowIdentifierUpdate = false, bool shouldDirtyScheme = true)
+        {
+            return scheme.SetDataOnEntry(CodeGen.CodeGenUtils.Context, entry, attributeName, value, allowIdentifierUpdate,
+                shouldDirtyScheme);
+        }
+    }
+}
